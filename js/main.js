3b9dd4b3c246a1ee0b43551da2c268d42bb7002a
tonyideng <tonydeng@Tonys-Mac-Pro.local>
2016-10-09 20:32:24 -0400

    updated overall look and feel of landing page.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 
 //animation of headers on each page
  $(document).ready(function() {
-     $('.introduction').fadeIn({queue: false, duration: 1800});
-     $('.introduction').animate({'margin-top': '57px'}, 1600);
+     $('.introduction').fadeIn({queue: false, duration: 1300});
+     $('.introduction').animate({'margin-top': '140px'}, 1300);
   });
 
 //animation of body content on home-page
-$(document).ready(function() {
+/*$(document).ready(function() {
     $('.project-container').fadeIn({queue: false, duration: 1500});
     $('.project-container').animate({'margin-top': '40px'}, 1600);
- });
+ });*/
 
 //animation of body content on each project pages
  $(document).ready(function() {
@@ -17,11 +17,24 @@ $(document).ready(function() {
      $('.item-container').animate({'margin-top': '40px'}, 1600);
   });
 
+  //fading in sub-intro delay
+  $(".project-intro").hide(0).delay(2800).fadeIn(1300);
+
+  //clicking to reveal projects
+  $("#current-projects").click(function(){
+        $(".project-container").fadeToggle(1500);
+    });
+
+    $(".back-to-top").click(function(){
+          $(".project-container").fadeOut(1500);
+      });
+
+
 
 //animation of anchor scrolling
  $(document).on('click', 'a', function(event){
 
     $('html,body').animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
+    }, 1000);
 });
