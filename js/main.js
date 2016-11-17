bb7bf0df9fe535795aa1aacfae69e915a5a6ccea
tonyideng <tonydeng@Tonys-MacBook-Pro.local>
2016-11-17 09:37:23 -0500

    fixed speed based on feedback

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
 //animation of headers on each page
  $(document).ready(function() {
-     $('.introduction').fadeIn({queue: false, duration: 1300});
-     $('.introduction').animate({'margin-top': '140px'}, 1300);
+     $('.introduction').fadeIn({queue: false, duration: 900});
+     $('.introduction').animate({'margin-top': '140px'}, 900);
   });
 
 //animation of body content on home-page
@@ -13,25 +13,25 @@
 
 //animation of body content on each project pages
  $(document).ready(function() {
-     $('.item-container').fadeIn({queue: false, duration: 1500});
-     $('.item-container').animate({'margin-top': '40px'}, 1600);
+     $('.item-container').fadeIn({queue: false, duration: 900});
+     $('.item-container').animate({'margin-top': '40px'}, 900);
   });
 
   //fading in sub-intro delay
-  $('.project-intro').hide().delay(2400).queue(function(next) {
-      $(this).fadeIn({duration:1300, queue:false}).animate({'margin-top':  '40px'}, {duration:1300, queue:false});
+  $('.project-intro').hide().delay(1200).queue(function(next) {
+      $(this).fadeIn({duration:1000, queue:false}).animate({'margin-top':  '40px'}, {duration:1000, queue:false});
    });
 
 
   //clicking to reveal projects
   $("#current-projects").click(function(){
         $(".past-project-container").fadeOut(1000);
-        $(".project-container").fadeIn(1500);
+        $(".project-container").fadeIn(1300);
     });
 
   $("#past-projects").click(function(){
         $(".project-container").fadeOut(1000);
-        $(".past-project-container").fadeIn(1500);
+        $(".past-project-container").fadeIn(1300);
     });
 
   /*$(".back-to-top").click(function(){
