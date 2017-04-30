75e308f89d649c3c770ac37c30b5b9d1e75eb6e0
tonyideng <tdengdesign@gmail.com>
2017-04-30 00:52:39 -0400

    updated a bunch of stuff for about.html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 
 //animation of headers on each page
  //$(document).ready(function() {
-     $('.introduction, .about-me').fadeIn({queue: false, duration: 900});
-     $('.introduction, .about-me').animate({'margin-top': '90px'}, 900);
+     $('.introduction').fadeIn({queue: false, duration: 900});
+     $('.introduction').animate({'margin-top': '90px'}, 900);
 //  });
 
 $('.intro-hero').fadeIn({queue: false, duration: 1000});
 $('.intro-hero').animate({'margin-left': '-10px'}, 900);
+
+$('.about-me').fadeIn({queue: false, duration: 900});
+$('.about-me').animate({'margin-top': '50px'}, 900);
 
 //animation of body content on home-page
 /*$(document).ready(function() {
