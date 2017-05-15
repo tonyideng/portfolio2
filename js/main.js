8155bd4ca782e9a7c59654d3b533e0771a5f2085
tonyideng <tdengdesign@gmail.com>
2017-05-15 11:57:28 -0400

    added in scrolling sticky navbar

--- js/main.js
+++ js/main.js
@@ -104,6 +104,43 @@ $('#view-process').click(function(){
 });
 
 
+$(window).bind('scroll', function () {
+
+    if ($(window).scrollTop() > 150)
+        $('.mainmenu').addClass('nav-down');
+    else
+        $('.mainmenu').removeClass('nav-down');
+});
+
+
+// Hide Header on on scroll down
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.mainmenu').outerHeight();
+
+$(window).scroll(function(event){
+
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.mainmenu').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.mainmenu').removeClass('nav-up');
+        }
+    }
+
+    lastScrollTop = st;
+});
+
 
 //zoom image on click
 $('.process_pic').click(function() {
